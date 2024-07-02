5cf00a177679cc3822137c99ad73310a403014ba
gitcoder <kuntalmajee338@gmail.com>
2024-07-02 18:12:52 +0530

    database conncected

--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+const mongoose = require("mongoose");
+const connectToDatabase = require('./db')
+
 const port = 8080;
 
+
+app.use(connectToDatabase);
 
 app.listen(port, () => {
     console.log(`listing the port ${port}`)
