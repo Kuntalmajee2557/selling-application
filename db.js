9a07d39c354266598546f0268b515cf54379c252
gitcoder <kuntalmajee338@gmail.com>
2024-07-02 18:12:28 +0530

    database conncected

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+main()
+.then((res) => {
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/sell-app');
+}
+
+module.exports = main;
