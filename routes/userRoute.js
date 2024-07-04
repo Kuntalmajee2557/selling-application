51a5cf562e9d3c629c2c0111f27dbeb47833cd47
gitcoder <kuntalmajee338@gmail.com>
2024-07-04 10:04:48 +0530

    fixing req

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,30 @@
 const express = require('express');
 const router = express.Router();
 
+//database connection
+const mongoose = require("mongoose");
+const connectToDatabase = require('./../db');
+
 router.get('/signup', (req, res) => {
     res.send('user signup');
 })
+
+router.post('/signup', (req, res) => {
+    const newUser = req.body;
+    console.log(newUser);
+    res.status(200).json({massage: "user created", newUser});
+})
+
+router.get('/login', (req, res) => {
+    res.send('user login');
+})
+
+router.post('/login', (req, res) => {
+    const {newUser} = req.body;
+    console.log(newUser);
+})
+
+
 
 router.get('/:id/profile', (req, res) => {
     res.send('user profile');
