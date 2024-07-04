2c5ee88aaccdf37ac3104123c81c5e66e4d861b9
gitcoder <kuntalmajee338@gmail.com>
2024-07-04 09:27:31 +0530

    body-parser added

--- server.js
+++ server.js
@@ -2,31 +2,31 @@
 const express = require("express");
 const app = express();
 
-
-const path = require("path");
-
-const mongoose = require("mongoose");
-const connectToDatabase = require('./db');
+const port = 8080;
 
 const userRoute = require("./routes/userRoute.js");
 const sellerRoute = require("./routes/sellerRoute.js");
 
+//define path
+const path = require("path");
 
-const port = 8080;
+//to connect database
+const mongoose = require("mongoose");
+const connectToDatabase = require('./db');
 
+app.use(express.json());
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
 
 app.use('/user', userRoute);
 app.use('/seller', sellerRoute);
-
-
 
 app.get('/', (req, res) => {
     res.send('hii');
 })
 
-
+//database connection
 app.use(connectToDatabase);
-
 
 app.listen(port, () => {
     console.log(`listing the port ${port}`)
