383b440272bd34a01245e42aff7b8c773b183a15
gitcoder <kuntalmajee338@gmail.com>
2024-07-04 22:31:42 +0530

    singup and login complete

--- routes/sellerRoute.js
+++ routes/sellerRoute.js
@@ -1,12 +1,65 @@
 import express from 'express';
 const router = express.Router();
 
+import Seller from './../models/sellerModel.js'
+
 router.get('/signup', (req, res) => {
     res.send("seller signup");
 })
 
-router.get('/:id/profile', (req, res) => {
-    res.send('seller profile');
+router.post('/signup', async (req, res) => {
+    try{
+        const data = req.body;
+        console.log(data);
+    
+        //find this username is already exist or not
+        const existSeller = await Seller.findOne({username: data.username});
+    
+        if(existSeller){
+            return res.status(400).json({error: "seller already exist"});
+        }
+    
+        const newSeller = new Seller(data);
+        const seller = await newSeller.save().then(seller => console.log(seller));
+    
+    
+        res.status(200).json({massage: "seller created", newSeller});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error : "Internal server error"});
+    }
 })
+
+router.get('/login', (req, res) => {
+    res.send("seller login");
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(data);
+
+        const existSeller = await Seller.findOne({ username: data.username });
+
+        if (!existSeller) {
+            return res.status(400).json({ error: "seller not found! singup first" });
+        }
+
+        if(data.password != existSeller.password){
+            return res.status(400).json({ error: "wrong password" });
+        }
+
+        res.status(400).json(existSeller);
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+    
+})
+
+
 
 export default router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,30 +6,73 @@ const router = express.Router();
 import mongoose from "mongoose";
 import connectToDatabase from './../db.js';
 
-import User from'./../models/userModel.js';
+import User from './../models/userModel.js';
 
 
 router.get('/signup', (req, res) => {
     res.send('user signup');
 })
 
-router.post('/signup', (req, res) => {
-    const newUser = req.body;
-    console.log(newUser);
-    res.status(200).json({massage: "user created", newUser});
+router.post('/signup', async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(data);
+
+        //find this username is already exist or not
+        const existUser = await User.findOne({ username: data.username });
+
+        if (existUser) {
+            return res.status(400).json({ error: "user already exist" });
+        }
+
+        const newUser = new User(data);
+        const user = await newUser.save().then(user => console.log(user));
+
+
+        res.status(200).json({ massage: "user created", newUser });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+
 })
 
 router.get('/login', (req, res) => {
     res.send('user login');
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(data);
 
+        const existUser = await User.findOne({ username: data.username });
 
+        if (!existUser) {
+            return res.status(400).json({ error: "user not found! singup first" });
+        }
 
+        if(data.password != existUser.password){
+            return res.status(400).json({ error: "wrong password" });
+        }
 
-router.get('/:id/profile', (req, res) => {
-    res.send('user profile');
+        res.status(400).json(existUser);
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+    
 })
+
+
+
+
+
+
+
 
 
 export default router;
