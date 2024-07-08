e5eadb1a1ff57a5a02cdec2217c5682e7da9803d
gitcoder <kuntalmajee338@gmail.com>
2024-07-08 21:26:58 +0530

    bycript compare

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,18 @@ const userSchema = new mongoose.Schema({
     }]
   });
 
+
+  userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+  }
+
+
 //compile models from the shema
 const User = mongoose.model("User", userSchema);
 export default User;
